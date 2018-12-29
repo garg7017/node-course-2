a9e27831011ab389c867e58550969ecfe93bd472
unknown <lgarg@Lokesh-Garg.velocity.com>
2018-12-29 19:19:35 +0530

    modifying

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;	
-mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost:27017/TodoAppNew");
+// mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost:27017/TodoAppNew");
+
+mongoose.connect("mongodb://localhost:27017/TodoAppNew");
 
 
 module.exports = {
